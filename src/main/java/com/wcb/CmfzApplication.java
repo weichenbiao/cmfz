c988cf327486952f9e9c5bd9f9b50d96ae1f7020
wcb <1493140970@qq.com>
2019-10-14 15:31:18 +0800

    v1.1

--- src/main/java/com/wcb/CmfzApplication.java
+++ src/main/java/com/wcb/CmfzApplication.java
@@ -10,6 +10,7 @@ public class CmfzApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CmfzApplication.class, args);
+        System.out.println();
     }
 
 }
